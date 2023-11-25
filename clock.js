a930d8dc516bef1b4ca3c7960426de9d02a73a8d
Gelomiko <143159953+Gelomiko@users.noreply.github.com>
2023-11-26 03:56:49 +1000

    updates to files

--- clock.js
+++ clock.js
@@ -1,9 +1,9 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+const hourhand = document.querySelector('.hour-hand');
 
 function setClockFace(){
-    //now Const variable is not global its local to the method
+    //now constant var is not global only local to the variable
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds /60) * 360) + 90;
@@ -14,16 +14,16 @@ function setClockFace(){
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
     const hours = now.getHours();
-    const hourDegrees = ((hours /60) * 360) + 90;
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    const hoursDegrees = ((hours /60) * 360) + 90;
+    hourhand.style.transform = `rotate(${hoursDegrees}deg)`;
 
     var time = padTwo(hours) + ":" + padTwo(minutes) + ":" + padTwo(seconds);
     const digitalClockFace = document.querySelector('.time');
-    digitalClockFace,innerHTML = time;
+    digitalClockFace.innerHTML = time;
 }
 
 function padTwo(number){
     return (number < 10 ? '0' : '') + number;
 }
-// 1000 ms is 1 second
-setInterval(setClockFace,1000);
+//1000 millisec is equal to 1 second
+setInterval(setClockFace,1000)
